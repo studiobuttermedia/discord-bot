840e32123cee3a753ebd113af8fd70623719bf29
Butter <55838591+studiobuttermedia@users.noreply.github.com>
2022-08-18 22:16:25 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import { config } from 'dotenv';
-import { Client } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 config();
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages']})
+const client = new Client({ 
+    intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages
+    ]
+});
+
 const TOKEN = process.env.BOT_TOKEN
 
-client.login(TOKEN)
+client.login(TOKEN);
+
+client.on('ready', () => {
+    console.log('Skybot is charged up and ready to fly');
+});
+
+client.on('messageCreate', (message) => {
+    console.log(message.content);
+});
